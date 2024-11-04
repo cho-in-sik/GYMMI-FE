0997de756d717ca987f75e73f7e76332962e7dab
seunghyeon77 <jdyjsh77@naver.com>
2024-11-04 23:44:44 +0900

    refactor: 워크스페이스 메인 제목 컴포넌트로 변경

---
+++ src/app/(afterLogin)/workspace/[workspaceId]/_components/WorkspaceTitle.tsx
@@ -0,0 +1,13 @@
+type TWorkspaceTitle = {
+  name: string;
+  workout: boolean;
+};
+
+function WorkspaceTitle({ name, workout }: TWorkspaceTitle) {
+  return (
+    <div className={`flex items-end ${!workout ? 'mb-8' : 'mb-3'}`}>
+      <h1 className='font-galmuri text-[28px] ml-2'>{name}</h1>
+    </div>
+  );
+}
+export default WorkspaceTitle;
