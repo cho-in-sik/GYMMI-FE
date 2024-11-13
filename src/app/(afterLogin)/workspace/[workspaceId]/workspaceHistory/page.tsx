0a3cbe1f6a03bafbfe3fc38951c2470f01d5a893
seunghyeon77 <jdyjsh77@naver.com>
2024-11-13 13:52:12 +0900

    refactor: 히스토리 페이지 리팩토링

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceHistory/page.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceHistory/page.tsx
@@ -1,61 +1,31 @@
 'use client';
 
-import radius from '@/../public/svgs/workspace/workspaceHistory/radius.svg';
-import radiusClicked from '@/../public/svgs/workspace/workspaceHistory/radiusClicked.svg';
-import detailHistoryRadius from '@/../public/svgs/workspace/workspaceHistory/detailHistoryRadius.svg';
-import arrow from '@/../public/svgs/workspace/workspaceHistory/arrow.svg';
-import verticalLine from '@/../public/svgs/workspace/verticalLine.svg';
-import detailVertical from '@/../public/svgs/workspace/workspaceHistory/detailVertical.svg';
-import detailVerticalIsToggled from '@/../public/svgs/workspace/workspaceHistory/detailVerticalIsToggled.svg';
-import noGroup from '@/../public/svgs/noGroup.svg';
-
-import speechBubble1 from '@/../public/svgs/workspace/speechBubble/speechBubble1.svg';
-import speechBubble2 from '@/../public/svgs/workspace/speechBubble/speechBubble2.svg';
-import speechBubble3 from '@/../public/svgs/workspace/speechBubble/speechBubble3.svg';
-
-import { workspaceHistoryData } from '@/constants/queryKey';
-import { historyDetails, workspaceHistorys } from '@/api/workspace';
-import { cheerUpMessages } from '@/constants/cheerUpMessage';
-
-import WorkspaceTitle from '@/app/(afterLogin)/workspace/[workspaceId]/_components/WorkspaceTitle';
-import WorkspaceGimmi from '@/app/(afterLogin)/workspace/[workspaceId]/_components/WorkspaceGimmi';
-
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 
-type TScoreData = {
-  id: number;
-  label: string;
-  value: number | string;
-};
+import radius from '@/../public/svgs/workspace/workspaceHistory/radius.svg';
+import radiusClicked from '@/../public/svgs/workspace/workspaceHistory/radiusClicked.svg';
+import detailHistoryRadius from '@/../public/svgs/workspace/workspaceHistory/detailHistoryRadius.svg';
+import arrow from '@/../public/svgs/workspace/workspaceHistory/arrow.svg';
+import detailVertical from '@/../public/svgs/workspace/workspaceHistory/detailVertical.svg';
+import detailVerticalIsToggled from '@/../public/svgs/workspace/workspaceHistory/detailVerticalIsToggled.svg';
+import noGroup from '@/../public/svgs/noGroup.svg';
 
-type THistorys = {
-  id: number;
-  isApproved: string;
-  createdAt: string;
-  sumOfScore: number;
-};
+import { workspaceHistoryData } from '@/constants/queryKey';
+import { cheerUpMessages } from '@/constants/cheerUpMessage';
+import { historyDetails, workspaceHistorys } from '@/api/workspace';
 
-interface IQueryTypes {
-  userId: number;
-  name: string;
-  workout: boolean;
-  achievementScore: number;
-}
+import WorkspaceScoreBoard from './_components/workspaceScoreBoard';
+import WorkspaceGimmiTitle from './_components/WorkspaceGimmiTitle';
 
-type TDetailHistorys = {
-  id: number;
-  count: number;
-  mission: string;
-  totalScore: number;
-};
-
-type THistoryDetails = {
-  id: number;
-  details: TDetailHistorys[];
-};
+import type {
+  THistorys,
+  TQueryTypes,
+  TDetailHistorys,
+  THistoryDetails,
+} from '@/types/workspaceHistory';
 
 const cheerUpMessage = cheerUpMessages;
 
@@ -63,10 +33,11 @@ function Page() {
   const workspaceId = useParams();
   const workspaceIdNumber = Number(workspaceId.workspaceId);
 
-  const [workoutHistoryIds, setWorkoutHistoryIds] = useState<number[]>([]);
   const [workoutHistoryId, setWorkoutHistoryId] = useState<number>(0);
+  const [workoutHistoryIds, setWorkoutHistoryIds] = useState<number[]>([]);
+
   const [randomMessage, setRandomMessage] = useState(``);
-  const [queryData, setQueryData] = useState<IQueryTypes | null>(null);
+  const [queryData, setQueryData] = useState<TQueryTypes | null>(null);
 
   const [workoutMissions, setWorkoutMissions] = useState<THistoryDetails[]>([]);
   const [isWorkoutHistoryDetail, setIsWorkoutHistoryDetail] = useState(false);
@@ -132,24 +103,6 @@ function Page() {
     },
   });
 
-  const scoreDatas = [
-    {
-      id: 1,
-      label: '일일 최고 운동점수',
-      value: `${workspaceHistoryDatas?.data.totalContributedScore}`,
-    },
-    {
-      id: 2,
-      label: '누적 운동 기록 횟수',
-      value: workspaceHistoryDatas?.data.bestDailyScore,
-    },
-    {
-      id: 3,
-      label: '1등과의 격차',
-      value: workspaceHistoryDatas?.data.gabScoreFromFirst,
-    },
-  ];
-
   const { data: workspaceHistoryDetail } = useQuery({
     queryKey: [
       [
@@ -193,57 +146,15 @@ function Page() {
 
   return (
     <div className='h-screen'>
-      {queryData && (
-        <>
-          <WorkspaceTitle name={queryData.name} workout={queryData.workout} />
-          <div className='flex justify-center items-end gap-x-6 h-48 mb-5'>
-            <WorkspaceGimmi
-              workout={queryData.workout}
-              achievementScore={queryData.achievementScore}
-            />
-            <div className='pb-28 relative'>
-              <span className='text-[#4B5563] text-[10px] absolute top-5 right-5 left-6'>
-                {randomMessage}
-              </span>
-              <Image src={speechBubble3} alt='speechBubble3' />
-              <Image
-                className='ml-3 my-2'
-                src={speechBubble2}
-                alt='speechBubble2'
-              />
-              <Image src={speechBubble1} alt='speechBubble1' />
-            </div>
-          </div>
-        </>
-      )}
-      <div className='h-14 bg-white rounded-lg mb-4'>
-        <div className='w-full' defaultValue='totalScore'>
-          <div className='flex gap-x-4 pl-7 pt-2 justify-items-center'>
-            <div className='flex flex-col items-center'>
-              <span className='text-[#9C9EA3] text-[10px]'>총 점수</span>
-              <span className='text-[#1F2937] text-base'>
-                {workspaceHistoryDatas?.data.totalContributedScore}점
-              </span>
-            </div>
-            <Image src={verticalLine} alt='verticalLine' />
-            {scoreDatas.map((scoreData: TScoreData) => {
-              return (
-                <div
-                  className='flex flex-col items-center justify-center'
-                  key={scoreData.id}
-                >
-                  <span className='text-[#9C9EA3] text-[10px]'>
-                    {scoreData.label}
-                  </span>
-                  <span className='text-[#1F2937] text-sm pt-1'>
-                    {scoreData.value}점
-                  </span>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
+      <WorkspaceGimmiTitle
+        queryData={queryData}
+        randomMessage={randomMessage}
+      />
+
+      <WorkspaceScoreBoard
+        workspaceHistoryDatas={workspaceHistoryDatas?.data}
+      />
+
       <div className='bg-white rounded-lg relative min-h-80 max-h-96 overflow-y-auto'>
         <div className='w-full pt-2 pb-5' defaultValue='myHistory'>
           <span className='text-[#9CA3AF] text-xs pl-5'>
