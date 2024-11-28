fa9062b6a7c250ccfd22d127407e1c3c20fcc370
조인식 <imsif42@naver.com>
2024-11-28 20:38:41 +0900

    fix: 타입 선언 띄어쓰기 제거

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceHistory/_components/WorkspaceScoreBoard.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceHistory/_components/WorkspaceScoreBoard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 import verticalLine from '@/../public/svgs/workspace/verticalLine.svg';
 import type { TScoreData } from '@/types/workspaceHistory';
-import type { TWorkspaceHistory } from '@/types/\bworkSpace';
+import type { TWorkspaceHistory } from '@/types/workSpace';
 
 interface WorkspaceScoreBoardProps {
   workspaceHistoryDatas: TWorkspaceHistory;
@@ -29,26 +29,26 @@ function WorkspaceScoreBoard({
     },
   ];
   return (
-    <div className='h-14 bg-white rounded-lg mb-4'>
-      <div className='w-full' defaultValue='totalScore'>
-        <div className='flex gap-x-4 pl-7 pt-2 justify-items-center'>
-          <div className='flex flex-col items-center'>
-            <span className='text-[#9C9EA3] text-[10px]'>총 점수</span>
-            <span className='text-[#1F2937] text-base'>
+    <div className="h-14 bg-white rounded-lg mb-4">
+      <div className="w-full" defaultValue="totalScore">
+        <div className="flex gap-x-4 pl-7 pt-2 justify-items-center">
+          <div className="flex flex-col items-center">
+            <span className="text-[#9C9EA3] text-[10px]">총 점수</span>
+            <span className="text-[#1F2937] text-base">
               {workspaceHistoryDatas?.totalContributedScore}점
             </span>
           </div>
-          <Image src={verticalLine} alt='verticalLine' />
+          <Image src={verticalLine} alt="verticalLine" />
           {scoreDatas.map((scoreData: TScoreData) => {
             return (
               <div
-                className='flex flex-col items-center justify-center'
+                className="flex flex-col items-center justify-center"
                 key={scoreData.id}
               >
-                <span className='text-[#9C9EA3] text-[10px]'>
+                <span className="text-[#9C9EA3] text-[10px]">
                   {scoreData.label}
                 </span>
-                <span className='text-[#1F2937] text-sm pt-1'>
+                <span className="text-[#1F2937] text-sm pt-1">
                   {scoreData.value}점
                 </span>
               </div>
