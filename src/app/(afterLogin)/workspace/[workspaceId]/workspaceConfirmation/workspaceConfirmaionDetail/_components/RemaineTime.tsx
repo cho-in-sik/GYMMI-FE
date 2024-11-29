f0092bd38d816aef2ac9d264485c560d0ab4f0ca
seunghyeon77 <jdyjsh77@naver.com>
2024-11-29 14:19:54 +0900

    refactor: 글씨 '시간'으로 변경

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/workspaceConfirmaionDetail/_components/RemaineTime.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/workspaceConfirmaionDetail/_components/RemaineTime.tsx
@@ -15,20 +15,20 @@ const RemaineTime = ({ deadline }: RemaineTimeProps) => {
 
       if (remineTime <= 0) {
         setRemaineTime('00시 00분');
-        clearInterval(interval); // 종료 시간을 지나면 타이머를 중지
+        clearInterval(interval);
       } else {
         const hours = Math.floor(remineTime / (1000 * 60 * 60));
         const minutes = Math.floor(
           (remineTime % (1000 * 60 * 60)) / (1000 * 60)
         );
-        setRemaineTime(`${hours}시 ${minutes}분`);
+        setRemaineTime(`${hours}시간 ${minutes}분`);
       }
     }, 1000);
 
     return () => clearInterval(interval); // 컴포넌트가 사라질 때 타이머 종료
-  }, [deadline]); // `deadline` 값에 의존하여 `useEffect` 실행
+  }, [deadline]);
 
-  return <span>{remaineTime}</span>; // UI에 표시
+  return <span>{remaineTime}</span>;
 };
 
 export default RemaineTime;
