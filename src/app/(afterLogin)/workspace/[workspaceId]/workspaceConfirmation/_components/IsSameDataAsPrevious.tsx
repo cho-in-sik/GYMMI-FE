fef7766ee67c24bd0bb9805730e2b7630fefe9c7
seunghyeon77 <jdyjsh77@naver.com>
2024-12-20 13:44:12 +0900

    refactor: 운동 인증 게시 날짜에 요일 추가

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/_components/IsSameDataAsPrevious.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/_components/IsSameDataAsPrevious.tsx
@@ -15,13 +15,15 @@ export default function IsSameDateAsPrevious({
     index > 0 &&
     workoutConfirmationPages[index - 1]?.createdAt.substring(0, 10) ===
       workoutConfirmationPage.createdAt.substring(0, 10);
+  console.log(workoutConfirmationPage);
   return (
     <div>
       {!isSameDateAsPrevious && (
         <div className='flex justify-center'>
-          <div className='w-20 h-5 my-3 bg-[#F9FAFB] rounded flex justify-center'>
+          <div className='m-3 p-1 bg-[#F9FAFB] rounded flex justify-center'>
             <span className='text-[10px] text-[#4B5563]'>
-              {workoutConfirmationPage.createdAt.substring(0, 10)}
+              {workoutConfirmationPage.createdAt.substring(0, 10)}-
+              {workoutConfirmationPage.dayOfTheWeek.toLowerCase()}
             </span>
           </div>
         </div>
