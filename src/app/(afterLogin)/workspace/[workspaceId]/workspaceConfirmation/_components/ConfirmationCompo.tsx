2dcc31efaea16f47201017703959796d3be4d521
seunghyeon77 <jdyjsh77@naver.com>
2024-12-22 00:23:30 +0900

    refactor: 인증 컴포넌트 변경

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/_components/ConfirmationCompo.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/_components/ConfirmationCompo.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
 import { IWorkoutConfirmationPageProps } from '@/types/workoutConfirmation';
 import ConfirmationProfile from './ConfirmationProfile';
 import ConfirmationCompoObjection from './ConfirmationCompoObjection';
+import ConfirmationCompoConfirm from './ConfirmationCompoConfirm';
 
 interface IConfirmationCompoProps {
   workoutConfirmationPage: IWorkoutConfirmationPageProps;
@@ -27,48 +25,10 @@ export default function ConfirmationCompo({
             workoutConfirmationPage={workoutConfirmationPage}
           />
         ) : (
-          <div id={`confirmation-${workoutConfirmationPage.objectionId}`}>
-            <Link
-              href={{
-                pathname: `/workspace/${workspaceId}/workspaceConfirmation/workspaceConfirmaionDetail`,
-                query: {
-                  workoutConfirmationId:
-                    workoutConfirmationPage.workoutConfirmationId,
-                  isObjection:
-                    workoutConfirmationPage.isObjection ||
-                    workoutConfirmationPage.objectionId !== null,
-                  objectionId: workoutConfirmationPage.objectionId,
-                },
-              }}
-            >
-              <div
-                className={`${
-                  workoutConfirmationPage.isMine
-                    ? 'bg-[#FFEDA6]'
-                    : 'bg-[#FDFDFD]'
-                } w-[150px] h-[150px] pl-2 pt-1 rounded-lg drop-shadow-lg`}
-              >
-                <span className='text-[#1F2937] text-sm'>
-                  운동인증을 올렸어요!
-                </span>
-                <div className='flex gap-x-2 mt-1'>
-                  <div className='w-[105px] h-[105px] flex items-center justify-center relative'>
-                    {/* <Image
-                  src={
-                    workoutConfirmationPage.workoutConfirmationImageUrl
-                  }
-                  alt='workoutConfirmationImageUrl'
-                  loader={({ src }) => src}
-                  fill
-                  sizes='105px'
-                  className='object-cover'
-                /> */}
-                  </div>
-                  <div className='flex items-end'>...</div>
-                </div>
-              </div>
-            </Link>
-          </div>
+          <ConfirmationCompoConfirm
+            workoutConfirmationPage={workoutConfirmationPage}
+            workspaceId={workspaceId}
+          />
         )}
         <div
           className={`${
---
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/_components/ConfirmationCompoConfirm.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { IWorkoutConfirmationPageProps } from '@/types/workoutConfirmation';
+
+interface IConfirmationCompoConfirmProps {
+  workoutConfirmationPage: IWorkoutConfirmationPageProps;
+  workspaceId: number;
+}
+
+export default function ConfirmationCompoConfirm({
+  workoutConfirmationPage,
+  workspaceId,
+}: IConfirmationCompoConfirmProps) {
+  return (
+    <div id={`confirmation-${workoutConfirmationPage.objectionId}`}>
+      <Link
+        href={{
+          pathname: `/workspace/${workspaceId}/workspaceConfirmation/workspaceConfirmaionDetail`,
+          query: {
+            workoutConfirmationId:
+              workoutConfirmationPage.workoutConfirmationId,
+            isObjection:
+              workoutConfirmationPage.isObjection ||
+              workoutConfirmationPage.objectionId !== null,
+            objectionId: workoutConfirmationPage.objectionId,
+          },
+        }}
+      >
+        <div
+          className={`${
+            workoutConfirmationPage.isMine ? 'bg-[#FFEDA6]' : 'bg-[#FDFDFD]'
+          } w-[150px] h-[150px] pl-2 pt-1 rounded-lg drop-shadow-lg`}
+        >
+          <span className='text-[#1F2937] text-sm'>운동인증을 올렸어요!</span>
+          <div className='flex gap-x-2 mt-1'>
+            <div className='w-[105px] h-[105px] flex items-center justify-center relative'>
+              {/* <Image
+            src={
+              workoutConfirmationPage.workoutConfirmationImageUrl
+            }
+            alt='workoutConfirmationImageUrl'
+            loader={({ src }) => src}
+            fill
+            sizes='105px'
+            className='object-cover'
+          /> */}
+            </div>
+            <div className='flex items-end'>...</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
