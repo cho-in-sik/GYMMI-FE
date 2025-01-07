fa6bc80e961dcd51a296071ce69a51d90ff7847c
seunghyeon77 <jdyjsh77@naver.com>
2025-01-07 23:44:47 +0900

    feat: 테스크 페이지 스타일링 추가

--- src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
+++ src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
@@ -1,89 +1,94 @@
 'use client';
 
 import Image from 'next/image';
-import completeImage from '@/../public/svgs/completeImage.svg';
-import x from '@/../public/svgs/x.svg';
-import blackX from '@/../public/svgs/blackX.svg';
 import { useQuery } from '@tanstack/react-query';
-import { useParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
+import backArrow from '@/../public/svgs/backArrow.svg';
+import onePrize from '@/../public/svgs/workspace/workspaceHistory/onePrize.svg';
+import completeTask from '@/../public/images/completeTask.png';
 import { completeWorkspace } from '@/api/workspace';
-import { useState } from 'react';
-import ConfettiButton3 from '../_components/ConfettiButton';
+import useWorkoutIdFromParams from '@/hooks/workoutHistory/useWorkoutIdFromParams';
 
 export default function Page() {
   const router = useRouter();
-  const [open, setOpen] = useState(false);
-  const { workspaceId } = useParams();
-  const { data } = useQuery({
-    queryKey: ['complete', workspaceId],
-    queryFn: () => completeWorkspace(Number(workspaceId)),
-  });
-
-  const handleOpen = () => {
-    setOpen((v) => !v);
-  };
+  const workspaceId = useWorkoutIdFromParams();
+  // const { data: completeWorkspaceInfo } = useQuery({
+  //   queryKey: ['complete', workspaceId],
+  //   queryFn: () => completeWorkspace(workspaceId),
+  // });
 
   return (
-    <div
-      className={`w-full h-screen px-5 py-11 ${
-        open ? 'bg-[#EFF6FF]' : 'bg-[#60A5FA]'
-      }`}
-    >
-      <div className="mb-16" onClick={() => router.back()}>
-        {open ? (
-          <Image src={blackX} alt="black-x" />
-        ) : (
-          <Image src={x} alt="x" />
-        )}
+    <div className={`w-full h-screen px-5 py-11 bg-[#ffffff]`}>
+      <div className='mb-5' onClick={() => router.back()}>
+        <Image src={backArrow} alt='backArrow' />
       </div>
-      <div
-        className={`flex flex-col justify-center items-center mb-11 ${
-          open ? 'text-black' : 'text-white'
-        }`}
-      >
-        <h1 className="font-galmuri text-[26px]">워크스페이스 목표를</h1>
-        <h1 className="font-galmuri text-[26px] mb-5">모두 달성했어요!</h1>
-        <h6 className="text-xs">당첨된 테스크를 확인 후 팀원들과</h6>
-        <h6 className="text-xs">함께 테스크를 수행해보세요</h6>
-      </div>
-      <div className="flex flex-col justify-between items-center mb-8">
-        <div
-          className={`w-40 h-60 rounded-2xl flex justify-center items-center shadow-box-shadow mb-4 cursor-pointer ${
-            !open
-              ? 'animate-bounce bg-[#DBEAFE]'
-              : 'animate-flip-right bg-[#60A5FA]'
-          } `}
-          onClick={handleOpen}
-        >
-          {open ? (
-            <div className="text-white w-24 h-24 text-base text-center overflow-y-scroll">
-              {data?.data.pickedTask.task}
+      <div>
+        <div className='font-galmuri text-xl ml-5 flex flex-col gap-y-1'>
+          <span>워크스페이스 목표를</span>
+          <span>모두 달성했어요!</span>
+        </div>
+        <div className='w-full h-24 bg-[#F2F3F5] rounded-2xl mt-7 flex'>
+          <div className='flex flex-col gap-y-1 py-3 pl-6 justify-center'>
+            <span className='text-sm text-[#848D99]'>그룹 테스크</span>
+            <span className='text-2xl text-[#4B5563] font-bold'>
+              맛있는 밥 사주기
+            </span>
+          </div>
+          <div className='absolute right-2 top-[70px]'>
+            <Image src={completeTask} alt='completeTask' />
+          </div>
+        </div>
+        <div className='w-full bg-[#F2F3F5] rounded-2xl mt-3'>
+          <div className='px-6 py-5'>
+            <div className='text-xl text-[#9CA3AF] flex flex-col gap-y-1'>
+              <span>
+                <strong className='text-[#4B5563]'>최종순위</strong>를 확인 후
+              </span>
+              <span>테스크를 수행해보세요!</span>
             </div>
-          ) : (
-            <Image src={completeImage} alt="complete-card" />
-          )}
+            <div className='flex justify-between mt-8'>
+              <div className='w-40 flex flex-col items-center'>
+                <div className='flex'>
+                  <Image src={onePrize} alt='onePrize' className='w-14 h-14' />
+                  <div className='flex flex-col justify-center items-center'>
+                    <span className='text-xl text-[#4B5563] font-bold'>
+                      1등 닉네임
+                    </span>
+                    <span className='text-[10px] text-[#6B7280]'>
+                      총 1등 점수
+                    </span>
+                  </div>
+                </div>
+                <div className='w-full h-32 bg-[#fdfdff] rounded-t-lg mt-5 flex items-end justify-center'>
+                  <div className='text-sm text-[#D9D9D9] pb-0.5'>1</div>
+                </div>
+              </div>
+              <div className='w-40 flex flex-col items-center justify-end ml-1'>
+                <div className='flex flex-col items-center'>
+                  <span className='text-xl text-[#4B5563] font-bold'>
+                    4등 닉네임
+                  </span>
+                  <span className='text-[10px] text-[#6B7280]'>4등 점수</span>
+                </div>
+                <div className='w-full h-9 bg-[#fdfdff] rounded-t-lg mt-5 flex items-end justify-center'>
+                  <div className='text-sm text-[#D9D9D9] pb-0.5'>4</div>
+                </div>
+              </div>
+            </div>
+            <div className='mt-5 mb-8'>
+              <span className='text-[10px] text-[#6B7280] '>팀별 순위</span>
+              <div className='h-12 bg-[#ffffff] rounded-lg flex items-center'>
+                <div className='pl-5'>2</div>
+                <div className='w-full px-7 flex justify-between'>
+                  <span className='text-base text-[#7E7E7E]'>팀원 닉네임</span>
+                  <span className='text-base text-[#7E7E7E]'>팀별 점수</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <span
-          className={`text-[#fff] text-[10px] text-center ${
-            open && 'invisible'
-          }`}
-        >
-          카드를 눌러 테스크를 확인하세요
-        </span>
       </div>
-      <div className={`px-9 ${!open && 'invisible'}`}>
-        <span className="text-[10px] text-[#9CA3AF]">미당첨된 테스크</span>
-        <div className="text-xs mt-5">
-          {data?.data.tasks.map((item: any) => {
-            return (
-              <h6 className="mb-2.5" key={item.id}>
-                {item.task}
-              </h6>
-            );
-          })}
-        </div>
-      </div>
-      {open && <ConfettiButton3 />}
     </div>
   );
 }
