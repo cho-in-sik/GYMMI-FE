5de957ba7553a8a529ae06d703f8b5cc60372e3a
seunghyeon77 <jdyjsh77@naver.com>
2025-01-13 15:25:58 +0900

    refactor: 테스크 팀원 순위 컴포넌트로 변경

--- src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
+++ src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
@@ -7,11 +7,11 @@ import { useRouter } from 'next/navigation';
 import backArrow from '@/../public/svgs/backArrow.svg';
 import onePrize from '@/../public/svgs/workspace/workspaceHistory/onePrize.svg';
 import lastPrize from '@/../public/svgs/workspace/workspaceHistory/lastPrize.svg';
-import taskRank from '@/../public/svgs/workspace/taskRank.svg';
 import completeTask from '@/../public/images/completeTask.png';
 // import { completeWorkspace } from '@/api/workspace';
 import useWorkoutIdFromParams from '@/hooks/workoutHistory/useWorkoutIdFromParams';
 import OneLastRank from '../_components/OneLastRank';
+import TaskRankMenber from '../_components/TaskRankMenbers';
 
 const mockData = {
   task: '맛있는 밥 사주기',
@@ -108,29 +108,7 @@ export default function Page() {
             </div>
             <div className='mt-5 mb-8'>
               <span className='text-[10px] text-[#6B7280] '>팀별 순위</span>
-              {mockData.workers.map((worker) => (
-                <div
-                  key={worker.rank}
-                  className='h-12 bg-[#F9FAFB] shadow-md rounded-lg flex items-center mb-3 relative'
-                >
-                  <div className='ml-4'>
-                    <Image src={taskRank} alt='taskRank' />
-                    <span className='absolute left-[23px] top-4 text-xs text-[#6B7280]'>
-                      {worker.rank}
-                    </span>
-                  </div>
-                  <div className='w-full pl-4 pr-7 flex justify-between'>
-                    <span className='text-base text-[#7E7E7E]'>
-                      {' '}
-                      {worker.name}{' '}
-                    </span>
-                    <span className='text-base text-[#7E7E7E]'>
-                      {' '}
-                      {worker.contributeScore} P{' '}
-                    </span>
-                  </div>
-                </div>
-              ))}
+              <TaskRankMenber workers={mockData.workers} />
             </div>
           </div>
         </div>
