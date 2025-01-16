0e14b6ce627e216110ecb8cc19a4b5cb45ba2100
seunghyeon77 <jdyjsh77@naver.com>
2025-01-16 15:11:14 +0900

    feat: 이의신청 알림 목록 '완료됨' 카테고리 추가

--- src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/workspaceConfirmationObjectionList/page.tsx
+++ src/app/(afterLogin)/workspace/[workspaceId]/workspaceConfirmation/workspaceConfirmationObjectionList/page.tsx
@@ -9,10 +9,11 @@ import useWorkoutIdFromParams from '@/hooks/workoutHistory/useWorkoutIdFromParam
 import useInfiniteQuerys from '@/hooks/workoutConfirmation/ useInfiniteQuerys';
 import { workoutConfirmationObjectionLists } from '@/api/workspaceConfirmaion';
 import WorkoutConfirmationObjectionList from './_components/WorkoutConfirmationObjectionList';
+import NoDataUI from '../../../_components/NoDataUI';
 
 export default function Page() {
   const workspaceId = useWorkoutIdFromParams();
-  const [statusButton, setStatusButton] = useState('open');
+  const [statusButton, setStatusButton] = useState('inProgress');
   const [ref, inView] = useInView();
 
   const workoutConfirmationObjectionList = useInfiniteQuerys({
@@ -32,26 +33,30 @@ export default function Page() {
       <div className='flex my-4'>
         <ObjectionListButton
           comment='모두'
-          onClick={() => setStatusButton('open')}
-          isClick={statusButton === 'open'}
+          onClick={() => setStatusButton('inProgress')}
+          isClick={statusButton === 'inProgress'}
         />
         <ObjectionListButton
           comment='투표 안함'
           onClick={() => setStatusButton('incompletion')}
           isClick={statusButton === 'incompletion'}
         />
-        {/* <ObjectionListButton
+        <ObjectionListButton
           comment='완료됨'
-          onClick={() => setStatusButton('incompletion')}
-          isClick={statusButton === 'incompletion'}
-        /> */}
+          onClick={() => setStatusButton('closed')}
+          isClick={statusButton === 'closed'}
+        />
       </div>
-      <WorkoutConfirmationObjectionList
-        workoutConfirmationObjectionListPages={
-          workoutConfirmationObjectionListPages
-        }
-        workspaceId={workspaceId}
-      />
+      {workoutConfirmationObjectionListPages?.length === 0 ? (
+        <NoDataUI content='이의신청 알림이 없습니다.' />
+      ) : (
+        <WorkoutConfirmationObjectionList
+          workoutConfirmationObjectionListPages={
+            workoutConfirmationObjectionListPages
+          }
+          workspaceId={workspaceId}
+        />
+      )}
       <div ref={ref} style={{ height: 10 }} />
     </div>
   );
