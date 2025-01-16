f78a5ffd350b4128c41affd7e87c728bac7e49c5
seunghyeon77 <jdyjsh77@naver.com>
2025-01-16 15:42:01 +0900

    style: 워크스페이스 완료 페이지 주석 제거

--- src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
+++ src/app/(afterLogin)/workspace-complete/[workspaceId]/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 import backArrow from '@/../public/svgs/backArrow.svg';
 import onePrize from '@/../public/svgs/workspace/workspaceHistory/onePrize.svg';
 import lastPrize from '@/../public/svgs/workspace/workspaceHistory/lastPrize.svg';
-// import { completeWorkspace } from '@/api/workspace';
+import { completeWorkspace } from '@/api/workspace';
 import useWorkoutIdFromParams from '@/hooks/workoutHistory/useWorkoutIdFromParams';
 import OneLastRank from '../_components/OneLastRank';
 import TaskRankMenber from '../_components/TaskRankMenbers';
@@ -16,10 +16,10 @@ import TaskContent from '../_components/TaskContent';
 export default function Page() {
   const router = useRouter();
   const workspaceId = useWorkoutIdFromParams();
-  // const { data: completeWorkspaceInfo } = useQuery({
-  //   queryKey: ['complete', workspaceId],
-  //   queryFn: () => completeWorkspace(workspaceId),
-  // });
+  const { data: completeWorkspaceInfo } = useQuery({
+    queryKey: ['complete', workspaceId],
+    queryFn: () => completeWorkspace(workspaceId),
+  });
 
   return (
     <div className={`w-full h-full px-5 py-11 bg-[#EFF6FF]`}>
@@ -31,7 +31,7 @@ export default function Page() {
           <span>워크스페이스 목표를</span>
           <span>모두 달성했어요!</span>
         </div>
-        {/* <TaskContent task={completeWorkspaceInfo?.data.task} /> */}
+        <TaskContent task={completeWorkspaceInfo?.data.task} />
 
         <div className='w-full bg-[#FFFFFF] rounded-2xl mt-3'>
           <div className='px-6 py-5'>
@@ -42,26 +42,38 @@ export default function Page() {
               <span>테스크를 수행해보세요!</span>
             </div>
             <div className='flex justify-between mt-8'>
-              {/* <OneLastRank
+              <OneLastRank
                 rankPrize={onePrize}
                 name={completeWorkspaceInfo?.data.workers[0].name}
-                contributeScore={completeWorkspaceInfo?.data.workers[0].contributeScore}
+                contributeScore={
+                  completeWorkspaceInfo?.data.workers[0].contributeScore
+                }
                 rank={completeWorkspaceInfo?.data.workers[0].rank}
                 height='32'
-              /> */}
-              {/* <OneLastRank
+              />
+              <OneLastRank
                 rankPrize={lastPrize}
-                name={completeWorkspaceInfo?.data.workers[completeWorkspaceInfo?.data.workers.length - 1].name}
-                contributeScore={
-                  completeWorkspaceInfo?.data.workers[completeWorkspaceInfo?.data.workers.length - 1].contributeScore
+                name={
+                  completeWorkspaceInfo?.data.workers[
+                    completeWorkspaceInfo?.data.workers.length - 1
+                  ].name
                 }
-                rank={completeWorkspaceInfo?.data.workers[completeWorkspaceInfo?.data.workers.length - 1].rank}
+                contributeScore={
+                  completeWorkspaceInfo?.data.workers[
+                    completeWorkspaceInfo?.data.workers.length - 1
+                  ].contributeScore
+                }
+                rank={
+                  completeWorkspaceInfo?.data.workers[
+                    completeWorkspaceInfo?.data.workers.length - 1
+                  ].rank
+                }
                 height='9'
-              /> */}
+              />
             </div>
             <div className='mt-5 mb-8'>
               <span className='text-[10px] text-[#6B7280] '>팀별 순위</span>
-              {/* <TaskRankMenber workers={completeWorkspaceInfo?.data.workers} /> */}
+              <TaskRankMenber workers={completeWorkspaceInfo?.data.workers} />
             </div>
           </div>
         </div>
