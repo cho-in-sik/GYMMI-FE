1b3dc73d28608b90c3c14547d9ccc629c527682a
조인식 <imsif42@naver.com>
2025-01-18 17:23:46 +0900

    fix: 히드라 에러 수정

--- src/components/FCMComponent.tsx
+++ src/components/FCMComponent.tsx
@@ -11,19 +11,23 @@ export default function FCMComponent() {
   const { fcmToken, notificationPermissionStatus } = useSendPush();
 
   useEffect(() => {
-    // FCM 토큰 로컬스토리지에 저장
-
-    if (fcmToken) {
+    if (typeof window !== 'undefined' && fcmToken) {
+      // FCM 토큰 로컬스토리지에 저장
       localStorage.setItem('fcmToken', fcmToken);
       const token = localStorage.getItem('accessToken');
       if (token) {
-        const res = postFcmToken(fcmToken);
-        console.log(res);
+        postFcmToken(fcmToken)
+          .then((res) => console.log('FCM Token sent:', res))
+          .catch((err) => console.error('Error sending FCM Token:', err));
       }
     }
 
     // 알림 권한 요청
-    if (Notification.permission !== 'granted') {
+    if (
+      typeof window !== 'undefined' &&
+      'Notification' in window &&
+      Notification.permission !== 'granted'
+    ) {
       Notification.requestPermission().then((permission) => {
         if (permission === 'granted') {
           console.log('알림 권한 허용되었습니다.');
@@ -34,22 +38,24 @@ export default function FCMComponent() {
     }
 
     // 포그라운드 상태에서 수신된 푸시 메시지 처리
-    const messaging = getMessaging(firebaseApp);
-    const unsubscribe = onMessage(messaging, (payload) => {
-      console.log('포그라운드 메시지 수신:', payload);
-      const { title, body } = payload.notification || {};
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      const messaging = getMessaging(firebaseApp);
+      const unsubscribe = onMessage(messaging, (payload) => {
+        console.log('포그라운드 메시지 수신:', payload);
+        const { title, body } = payload.notification || {};
 
-      // if (Notification.permission === 'granted') {
-      //   new Notification(title || '알림', {
-      //     body: body || '내용 없음',
-      //     icon: '/images/basicIcon.png',
-      //   });
-      // }
-    });
+        if (Notification.permission === 'granted') {
+          new Notification(title || '알림', {
+            body: body || '내용 없음',
+            icon: '/images/basicIcon.png',
+          });
+        }
+      });
 
-    return () => {
-      unsubscribe(); // 메시지 구독 해제
-    };
+      return () => {
+        unsubscribe(); // 메시지 구독 해제
+      };
+    }
   }, [fcmToken]);
 
   return null;
