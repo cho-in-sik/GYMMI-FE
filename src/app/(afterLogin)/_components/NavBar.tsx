9d88fc079c98d4e80a17ce4b485edcf488f9b6ad
조인식 <imsif42@naver.com>
2025-01-21 15:59:46 +0900

    fix: 워크스페이스 네브바 css & 순서 수정

--- src/app/(afterLogin)/_components/NavBar.tsx
+++ src/app/(afterLogin)/_components/NavBar.tsx
@@ -33,36 +33,40 @@ export default function NavBar() {
     <div>
       {pathName.split('/').pop() === 'register' ? null : (
         <div className="w-full h-[96px] fixed bottom-0 flex justify-around items-start border-t-2 pt-3 bg-white">
-          <div className="flex flex-col items-center" onClick={handleCreate}>
-            <Image src={createGroup} alt="createGroupIcon" className="my-0.5" />
-            <span className="text-[6px] ">그룹만들기</span>
-          </div>
           <Link href={'/workspace-list/mygroup'}>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-2">
               <Image src={group} alt="groupIcon" className="my-0.5" />
-              <span className="text-[6px] mt-0.5">그룹페이지</span>
-            </div>
-          </Link>
-          <Link href={'/'}>
-            <div className="flex flex-col items-center">
-              <Image src={home} alt="homeIcon" className="my-0.5" />
-              <span className="text-[6px] ">홈</span>
+              <span className="text-[10px] mt-0.5">그룹페이지</span>
             </div>
           </Link>
           <Link href={'/photoCommunity'}>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-2">
               <Image
                 src={photoCommunity}
                 alt="photoCommunityIcon"
                 className="my-0.5"
               />
-              <span className="text-[6px] ">사진커뮤니티</span>
+              <span className="text-[10px] ">사진커뮤니티</span>
             </div>
           </Link>
+          <Link href={'/'}>
+            <div className="flex flex-col items-center gap-2">
+              <Image src={home} alt="homeIcon" className="my-0.5" />
+              <span className="text-[10px] ">홈</span>
+            </div>
+          </Link>
+          <div
+            className="flex flex-col items-center gap-2"
+            onClick={handleCreate}
+          >
+            <Image src={createGroup} alt="createGroupIcon" className="my-0.5" />
+            <span className="text-[10px] ">그룹만들기</span>
+          </div>
+
           <Link href={'/mypage/user'}>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-2">
               <Image src={myPage} alt="myPageIcon" className="my-0.5" />
-              <span className="text-[6px] ">마이페이지</span>
+              <span className="text-[10px] ">마이페이지</span>
             </div>
           </Link>
         </div>
