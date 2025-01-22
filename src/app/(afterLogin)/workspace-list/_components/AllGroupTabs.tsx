b5849edf92a072c1dd29d6753b4a153c4b6183bd
조인식 <imsif42@naver.com>
2025-01-23 00:03:52 +0900

    fix :qa

--- src/app/(afterLogin)/workspace-list/_components/AllGroupTabs.tsx
+++ src/app/(afterLogin)/workspace-list/_components/AllGroupTabs.tsx
@@ -56,6 +56,8 @@ export default function AllGroupTabs() {
     },
   });
 
+  console.log(currentWorkspaceId);
+
   const onSubmit = async (e: any) => {
     e.preventDefault();
     try {
@@ -114,41 +116,41 @@ export default function AllGroupTabs() {
 
   return (
     <>
-      <div className='mb-6'>
+      <div className="mb-6">
         <input
-          type='text'
-          placeholder='Search your group'
-          className='bg-slate-100 w-full h-10 rounded-3xl placeholder:text-[10px] p-4 pt-3'
+          type="text"
+          placeholder="Search your group"
+          className="bg-slate-100 w-full h-10 rounded-3xl placeholder:text-[10px] p-4 pt-3"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
-      <Tabs defaultValue='all'>
+      <Tabs defaultValue="all">
         <TabsList>
           <TabsTrigger
-            value='before-p'
+            value="before-p"
             id={workspaceList.prepare}
             onClick={handleTabChange}
           >
             진행전
           </TabsTrigger>
           <TabsTrigger
-            value='ing-p'
+            value="ing-p"
             id={workspaceList.inProgress}
             onClick={handleTabChange}
           >
             진행중
           </TabsTrigger>
           <TabsTrigger
-            value='all'
+            value="all"
             id={workspaceList.complete}
             onClick={handleTabChange}
           >
             모두
           </TabsTrigger>
         </TabsList>
-        <div className='border-b-2 mt-2 w-full mb-4'></div>
-        <TabsContent value='before-p' className='relative'>
+        <div className="border-b-2 mt-2 w-full mb-4"></div>
+        <TabsContent value="before-p" className="relative">
           {data?.pages[0].data.length !== 0 ? (
             <div>
               {data?.pages[0].data.map((item: any) => (
@@ -166,43 +168,43 @@ export default function AllGroupTabs() {
                     asChild
                     onClick={() => handleAlreadyIn(item.id)}
                   >
-                    <div className='w-full h-20 bg-[#FEF9C3] rounded-lg flex justify-between items-center px-3.5 my-6'>
-                      <h1 className='text-[22px]'>{item.name}</h1>
+                    <div className="w-full h-20 bg-[#FEF9C3] rounded-lg flex justify-between items-center px-3.5 my-6">
+                      <h1 className="text-[22px]">{item.name}</h1>
                       <div>
-                        <Image src={nextArrow} alt='next-arrow' />
+                        <Image src={nextArrow} alt="next-arrow" />
                       </div>
                     </div>
                   </DialogTrigger>
-                  <DialogContent className='w-9/12 rounded-lg h-44'>
+                  <DialogContent className="w-9/12 rounded-lg h-44">
                     <DialogTitle>
-                      <DialogHeader className='text-xs mb-2'>
+                      <DialogHeader className="text-xs mb-2">
                         비밀번호를 입력해주세요
                       </DialogHeader>
                     </DialogTitle>
-                    <DialogDescription className=''>
-                      <div className='flex justify-center items-center'>
+                    <DialogDescription className="">
+                      <div className="flex justify-center items-center">
                         <form onSubmit={onSubmit}>
                           <input
-                            type='number'
-                            placeholder='숫자 4자리를 입력해주세요.'
-                            className='bg-[#F3F4F6] w-full h-[41px] px-2 rounded-lg placeholder:text-[10px]'
+                            type="number"
+                            placeholder="숫자 4자리를 입력해주세요."
+                            className="bg-[#F3F4F6] w-full h-[41px] px-2 rounded-lg placeholder:text-[10px]"
                             value={password}
                             onChange={(e) => handlePassword(e)}
                           />
                           {error !== '' && (
-                            <span className='text-[8px] text-[#EF4444] pl-1'>
+                            <span className="text-[8px] text-[#EF4444] pl-1">
                               {error}
                             </span>
                           )}
-                          <div className='flex justify-around items-center border-t-[1px] -mx-6'>
+                          <div className="flex justify-around items-center border-t-[1px] -mx-6">
                             <DialogClose asChild>
-                              <span className='text-sm text-[#D1D5DB] py-2 px-12 border-r-[1px]'>
+                              <span className="text-sm text-[#D1D5DB] py-2 px-12 border-r-[1px]">
                                 cancel
                               </span>
                             </DialogClose>
                             <button
-                              type='submit'
-                              className='text-sm text-[#3B82F6] py-2 px-12'
+                              type="submit"
+                              className="text-sm text-[#3B82F6] py-2 px-12"
                             >
                               join
                             </button>
@@ -218,19 +220,19 @@ export default function AllGroupTabs() {
             <NoWorkspace />
           )}
         </TabsContent>
-        <TabsContent value='ing-p' className='relative'>
+        <TabsContent value="ing-p" className="relative">
           {data?.pages[0].data.length !== 0 ? (
             <div>
               {data?.pages[0].data.map((item: any) => (
                 <div
                   key={item.id}
-                  className='w-full h-20 bg-[#60A5FA] rounded-lg flex justify-evenly items-start px-3.5 flex-col my-6'
+                  className="w-full h-20 bg-[#60A5FA] rounded-lg flex justify-evenly items-start px-3.5 flex-col my-6"
                   onClick={() => handleAlreadyIn(item.id)}
                 >
-                  <h2 className='text-[22px] -mb-3 text-white'>{item.name}</h2>
+                  <h2 className="text-[22px] -mb-3 text-white">{item.name}</h2>
                   <Progress
-                    indicatorColor='bg-[#1E40AF]'
-                    className='h-1.5'
+                    indicatorColor="bg-[#1E40AF]"
+                    className="h-1.5"
                     value={(item.achievementScore / item.goalScore) * 100}
                   />
                 </div>
@@ -240,22 +242,22 @@ export default function AllGroupTabs() {
             <NoWorkspace />
           )}
         </TabsContent>
-        <TabsContent value='all' className='relative'>
+        <TabsContent value="all" className="relative">
           {data?.pages[0].data.length !== 0 ? (
             <div>
               {data?.pages[0].data.map((item: any) => (
                 <div key={item.key}>
                   {item.status === workspaceList.inProgress ? (
                     <div
-                      className='w-full h-20 bg-[#60A5FA] rounded-lg flex justify-evenly items-start px-3.5 flex-col my-6'
+                      className="w-full h-20 bg-[#60A5FA] rounded-lg flex justify-evenly items-start px-3.5 flex-col my-6"
                       onClick={() => handleAlreadyIn(item.id)}
                     >
-                      <h2 className='text-[22px] -mb-3 text-white'>
+                      <h2 className="text-[22px] -mb-3 text-white">
                         {item.name}
                       </h2>
                       <Progress
-                        indicatorColor='bg-[#1E40AF]'
-                        className='h-1.5'
+                        indicatorColor="bg-[#1E40AF]"
+                        className="h-1.5"
                         value={(item.achievementScore / item.goalScore) * 100}
                       />
                     </div>
@@ -270,33 +272,33 @@ export default function AllGroupTabs() {
                     >
                       <div
                         onClick={() => handleAlreadyIn(item.id)}
-                        className='w-full h-20 bg-[#FEF9C3] rounded-lg flex justify-between items-center px-3.5 my-6'
+                        className="w-full h-20 bg-[#FEF9C3] rounded-lg flex justify-between items-center px-3.5 my-6"
                       >
-                        <h1 className='text-[22px]'>{item.name}</h1>
+                        <h1 className="text-[22px]">{item.name}</h1>
                         <div>
-                          <Image src={nextArrow} alt='next-arrow' />
+                          <Image src={nextArrow} alt="next-arrow" />
                         </div>
                       </div>
-                      <DialogContent className='w-9/12 rounded-lg h-44 p-0'>
-                        <DialogHeader className='flex justify-end'>
-                          <DialogTitle className='text-xs'>
+                      <DialogContent className="w-9/12 rounded-lg h-44 p-0">
+                        <DialogHeader className="flex justify-end">
+                          <DialogTitle className="text-xs">
                             비밀번호를 입력해주세요
                           </DialogTitle>
                         </DialogHeader>
                         <DialogDescription>
-                          <div className='w-full h-full'>
+                          <div className="w-full h-full">
                             <form onSubmit={onSubmit}>
-                              <div className='mx-8'>
+                              <div className="mx-8">
                                 <input
-                                  type='number'
-                                  placeholder='숫자 4자리를 입력해주세요.'
-                                  className='bg-[#F3F4F6] w-full h-11 py-4 pl-4 rounded-lg placeholder:text-[10px]'
+                                  type="number"
+                                  placeholder="숫자 4자리를 입력해주세요."
+                                  className="bg-[#F3F4F6] w-full h-11 py-4 pl-4 rounded-lg placeholder:text-[10px]"
                                   value={password}
                                   onChange={(e) => handlePassword(e)}
                                 />
                               </div>
                               {error && (
-                                <span className='ml-8 text-[8px] text-[#EF4444]'>
+                                <span className="ml-8 text-[8px] text-[#EF4444]">
                                   {error}
                                 </span>
                               )}
@@ -306,14 +308,14 @@ export default function AllGroupTabs() {
                                 } flex justify-around items-center`}
                               >
                                 <DialogClose asChild>
-                                  <span className='text-sm text-[#D1D5DB]'>
+                                  <span className="text-sm text-[#D1D5DB]">
                                     cancel
                                   </span>
                                 </DialogClose>
 
                                 <button
-                                  type='submit'
-                                  className='text-sm text-[#3B82F6]'
+                                  type="submit"
+                                  className="text-sm text-[#3B82F6]"
                                 >
                                   join
                                 </button>
