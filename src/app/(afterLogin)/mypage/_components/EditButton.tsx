7d0ea4f47ffe5c547f678717d5998cf1c0e646f5
조인식 <imsif42@naver.com>
2025-01-23 22:35:15 +0900

    fix: 마이페이지 버튼 수정

--- src/app/(afterLogin)/mypage/_components/EditButton.tsx
+++ src/app/(afterLogin)/mypage/_components/EditButton.tsx
@@ -2,7 +2,7 @@
 
 export default function EditButton({ children, bg }: any) {
   return (
-    <div className="w-11/12 h-12 bg-main flex justify-center items-center rounded-lg absolute bottom-10 ml-5">
+    <div className="w-11/12 h-12 bg-main flex justify-center items-center rounded-full absolute bottom-10 ml-5">
       <button className="text-white">{children}</button>
     </div>
   );
